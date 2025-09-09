dd80b78c5b02fc6c36784f208a8b9073b20e9948
jackedwards1128 <jack.edwards@menloschool.org>
2025-09-08 22:54:05 -0700

    Commented code :)

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -19,39 +19,29 @@ public class MokshaPatam {
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
 
-        int rolls = 0;
-
-        int position = boardsize;
-
-//        System.out.println("Ladders:");
-//        for (int ladder[] : ladders) {
-//            System.out.println(ladder[0] + " -> " + ladder[1]);
-//        }
-//        System.out.println("Snakes:");
-//        for (int snake[] : snakes) {
-//            System.out.println(snake[0] + " -> " + snake[1]);
-//        }
-
-        // Create node array
+        // Create node array and map for destinations of nodes
+        // Note that our arrays are length  boardsize+1. This choice is so that nodes can hold the values of their
+        // index, rather than their index + 1 . This adds only constant time to the runtime,
+        // but makes the code much easier to read and comprehend.
         int[] nodes = new int[boardsize+1];
         int[] map = new int[boardsize+1];
 
         for (int i = 1; i <= boardsize; i++) {
-            // Set destinations for snakes/ladders
+            // Create map for ladders
             for (int ladder[] : ladders) {
                 if (ladder[0] == i) {
                     map[i] = ladder[1];
                 }
             }
             if (map[i] == 0) {
+                // Add snakes to map
                 for (int snake[] : snakes) {
                     if (snake[0] == i) {
                         map[i] = snake[1];
-                        System.out.println(snake[0]);
                     }
                 }
             }
-
+            // If no snake or ladder present, set the destination of the node equal to it's index
             if (map[i] == 0) {
                 map[i] = i;
             }
@@ -59,29 +49,43 @@ public class MokshaPatam {
             nodes[i] = i;
         }
 
-        System.out.println("hello");
-
+        // Create the queue, which is the primary data structure for breadth first search
         Queue<Integer> queue = new LinkedList<Integer>();
+
+        // Create more associative arrays which house whether the node has been explored (visited) and how many rolls
+        // it took to get to a given node (traveltime)
+        // Arrays are length boardsize + 1 for reasons described on line 23
         boolean[] visited = new boolean[boardsize+1];
         int[] traveltime = new int[boardsize+1];
 
+        // Add the start of the board to the queue, as the search needs a beginning to run
         queue.add(1);
 
+        // This loop will only finish if the board is impossible, as the winning spot will come up in all other cases
         while(!(queue.isEmpty())) {
+            // Get the head of the queue
             int current = queue.remove();
+            // Base case: if we are at the winning spot, return how many rolls it took to get to "here"
             if (current == boardsize) {
                 return traveltime[current];
             }
+            // Add the 6 possible rolls (1-6) from the current node to the queue of nodes to explore
             for(int i = 1; (i <= 6) && (current + i <= boardsize); i++) {
+                // If we haven't already been to this node
                 if (!visited[current+i]) {
+                    // Visit both the node and the node's destination, as if a ladder comes from this spot, we are
+                    // visiting both the top and bottom of the ladder in a single step
                     visited[current+i] = true;
                     visited[map[current+i]] = true;
+                    // Set the travel time of the visited node to 1 more than the current travel time (i.e "rolls"++)
                     traveltime[map[current+i]] = traveltime[current] + 1;
+                    // Add the to-be-explored node to the queue
                     queue.add(map[current+i]);
                 }
             }
         }
-
+        // If the queue empties, that means the winning spot was never encountered, which means we can return -1 to
+        // signify an impossible board
         return -1;
     }
 
